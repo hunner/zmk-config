3cb24870fe4eb2d3aa49447ac42b70eab54d43e6
Hunter Haugen <hunter@haugens.org>
2023-02-04 17:57:26 -0800

    Undo thumb renaming

--- zmk-nodefree-config/keypos_def/keypos_28keys.h
+++ zmk-nodefree-config/keypos_def/keypos_28keys.h
@@ -3,7 +3,7 @@
   ╭────────────────────┬────────────────────╮ ╭─────────────────────┬─────────────────────╮
   │  0   1   2   3   4 │  5   6   7   8   9 │ │ LT4 LT3 LT2 LT1 LT0 │ RT0 RT1 RT2 RT3 RT4 │
   │ 10  11  12  13  14 │ 15  16  17  18  19 │ │ LM4 LM3 LM2 LM1 LM0 │ RM0 RM1 RM2 RM3 RM4 │
-  ╰───╮ 20  21  22  23 │ 24  25  26  27 ╭───╯ ╰───╮ LB3 LB2 LB1 LB0 │ RB0 RB1 RB2 RB3 ╭───╯
+  ╰───╮ 20  21  22  23 │ 24  25  26  27 ╭───╯ ╰───╮ LB3 LB2 LB1 LH0 │ RH0 RB1 RB2 RB3 ╭───╯
       ╰─────────────────────────────────╯         ╰───────────────────────────────────╯ */
 
 #pragma once
@@ -32,12 +32,14 @@
 #define RM3 18
 #define RM4 19
 
-#define LB0 23  // left-bottom row
-#define LB1 22
+#define LB1 22  // left-bottom row
 #define LB2 21
 #define LB3 20
 
-#define RB0 24  // right-bottom row
-#define RB1 25
+#define RB1 25  // right-bottom row
 #define RB2 26
 #define RB3 27
+
+#define LH0 23  // left thumb key
+
+#define RH0 24  // right thumb key
